720b99b4d24a06168cdc4d5ecf423facd6aeab2c
pjonasson <88997326+pjonasson@users.noreply.github.com>
2021-12-15 14:26:36 -0600

    function to search for stock with user params. Display name and stock symbol of search with v-for

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <button v-on:click="search">Click Here to test!</button>
+    <input type="text" v-model="searchStock" />
+    <button v-on:click="search">Search for Stock</button>
+    <div>
+      <div v-for="stock in inquiredStock" v-bind:key="stock.id">
+        <h2>Stock Name: {{ stock.name }}</h2>
+        <h2>Stock Symbol: {{ stock.symbol }}</h2>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,12 +21,18 @@ export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      searchStock: "",
+      inquiredStock: [],
     };
   },
   created: function () {},
   methods: {
     search: function () {
-      axios.get("http://localhost:3000/stocks/aa").then((response) => console.log("Test", response.data));
+      var searchParam = this.searchStock;
+      axios.get("http://localhost:3000/stocks/" + searchParam).then((response) => {
+        this.inquiredStock = response.data;
+        console.log(response.data);
+      });
     },
   },
 };
