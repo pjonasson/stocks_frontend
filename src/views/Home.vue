fd2c382f516fdcca85a24ee38a9af338537e1b33
pjonasson <88997326+pjonasson@users.noreply.github.com>
2021-12-15 15:23:32 -0600

    Added v-if for empty search result and display message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,14 @@
     <h1>{{ message }}</h1>
     <input type="text" v-model="searchStock" />
     <button v-on:click="search">Search for Stock</button>
-    <div>
-      <div v-for="stock in inquiredStock" v-bind:key="stock.id">
-        <h2>Stock Name: {{ stock.name }}</h2>
-        <h2>Stock Symbol: {{ stock.symbol }}</h2>
+    <div v-if="inquiredStock.length === 0">
+      <div>
+        <h1>There are no results for this search. Please try again.</h1>
       </div>
+    </div>
+    <div v-else-if="inquiredStock[0] != 1" v-for="stock in inquiredStock" v-bind:key="stock.id">
+      <h2>Stock Name: {{ stock.name }}</h2>
+      <h2>Stock Symbol: {{ stock.symbol }}</h2>
     </div>
   </div>
 </template>
@@ -22,7 +25,7 @@ export default {
     return {
       message: "Welcome to Vue.js!",
       searchStock: "",
-      inquiredStock: [],
+      inquiredStock: [1],
     };
   },
   created: function () {},
@@ -31,7 +34,7 @@ export default {
       var searchParam = this.searchStock;
       axios.get("http://localhost:3000/stocks/" + searchParam).then((response) => {
         this.inquiredStock = response.data;
-        console.log(response.data);
+        console.log(this.inquiredStock, response.data);
       });
     },
   },
