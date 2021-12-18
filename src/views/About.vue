0173d4149fd339f800e6674c20a86fa7fcde3f4e
pjonasson <88997326+pjonasson@users.noreply.github.com>
2021-12-18 16:20:28 -0600

    Switched method of loadData from created to async created to pull info on stock. Chart now loads.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <h3>{{ series[0].data }}</h3>
     <div>
       <apexchart v-if="loaded" width="1000" type="candlestick" :options="chartOptions" :series="series"></apexchart>
     </div>
@@ -14,6 +13,7 @@ import axios from "axios";
 export default {
   data: function () {
     return {
+      stockData: {},
       loaded: false,
       chartOptions: {
         chart: {
@@ -26,38 +26,46 @@ export default {
       series: [
         {
           name: "series-1",
-          data: [
-            {
-              x: "2021-11-17",
-              y: [6629.81, 6650.5, 6623.04, 6633.33],
-            },
-          ],
+          data: [],
         },
       ],
     };
   },
   components: {},
 
-  created: function () {
-    this.loadData();
+  async created() {
+    const stock = await axios.get(
+      "https://cloud.iexapis.com/stable/stock/FB/chart/30d?token=pk_b600aa212c854595ba1263155ea4c39a"
+    );
+    this.stock = stock.data;
+    console.log("Stock Info", stock.data);
+    this.stock.forEach((day) => {
+      var x = (this.series[0].data.x = day.date);
+      var y = (this.series[0].data.y = [day.open, day.high, day.low, day.close]);
+      this.series[0].data.push({ x, y });
+    });
+    this.loaded = true;
+
+    // this.loadData();
   },
   methods: {
     loadData: function () {
-      this.loaded = true;
-
       axios
         .get("https://cloud.iexapis.com/stable/stock/FB/chart/30d?token=pk_b600aa212c854595ba1263155ea4c39a")
         .then((response) => {
           this.stock = response.data;
           console.log("Facebook", this.stock);
-          this.stock.forEach((day) =>
-            this.series[0].data.push({ x: day.date, y: [day.open, day.high, day.close, day.low] })
-          );
-          console.log("Data for series", this.series[0].data);
+          this.stock.forEach((day) => {
+            var x = (this.series[0].data.x = day.date);
+            var y = (this.series[0].data.y = [day.open, day.high, day.close, day.low]);
+            this.series[0].data.push({ x, y });
+          });
         });
+      this.loaded = true;
     },
   },
 };
 </script>
 
-this.series[this.data].push()
+this.stock.forEach((day) => this.series[0].data.push({ x: day.date, y: [day.open, day.high, day.close, day.low] }) );
+console.log("Data for series", this.series[0].data);
